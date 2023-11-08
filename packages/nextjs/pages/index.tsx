9d05471bd244573638715be23107cb6151b1d0b9
Shiv Bhonde <shivbhonde04@gmail.com>
2023-11-08 16:23:43 +0300

    complete basic functionality

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -4,12 +4,12 @@ import { ArgumentTypeName } from "@pcd/pcd-types";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
+import { mainnet, useAccount, useSignMessage } from "wagmi";
 import { AddSubscriptionURL } from "~~/components/AddSubscriptionURL";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { SignMessageButton } from "~~/components/SignMessageButton";
 import { notification } from "~~/utils/scaffold-eth";
 
+const BAYC_ADDRESS = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D";
 const API_BASE_URL = "http://192.168.1.233:4000";
 
 const pcdArgs = {
@@ -51,8 +51,43 @@ const Home: NextPage = () => {
 
   const { query } = useRouter();
   const [messageToSign, setMessageToSign] = useState("");
+  const [subscriptionURL, setSubscriptionURL] = useState("");
+
+  const {
+    data: signMessageData,
+    error,
+    isLoading,
+    signMessageAsync,
+    isSuccess: isSigningMessageSuccessfull,
+  } = useSignMessage({
+    message: messageToSign,
+    onSuccess: () => {
+      generateSubscriptionURL();
+    },
+  });
 
   const proof = query && query.proof && JSON.parse(decodeURIComponent(query.proof as string));
+
+  const handleSign = async () => {
+    try {
+      await signMessageAsync();
+      notification.success("Message signed successfully");
+    } catch (e) {
+      notification.error("An Error happend while signing the message");
+      console.log("An Error happend while signing the message", e);
+      console.log("Error", error?.cause);
+    }
+  };
+
+  const generateSubscriptionURL = () => {
+    if (!proof) return;
+    const base64EncodedPaylod = window.btoa(
+      JSON.stringify({ account: address, signature: signMessageData, pcd: proof.pcd }),
+    );
+
+    const url = `${API_BASE_URL}/feeds/${mainnet.id}/${BAYC_ADDRESS}/${base64EncodedPaylod}`;
+    setSubscriptionURL(url);
+  };
 
   useEffect(() => {
     const getSigningMessage = async () => {
@@ -98,7 +133,14 @@ const Home: NextPage = () => {
           </div>
           {isConnected ? (
             <>
-              {messageToSign && <SignMessageButton message={messageToSign} />}
+              {messageToSign && !isSigningMessageSuccessfull && (
+                <button
+                  className={`btn btn-primary btn-outline ${isLoading ? "loading" : ""}`}
+                  onClick={() => handleSign()}
+                >
+                  {isLoading ? "Check Wallet" : "Sign Message"}
+                </button>
+              )}
               {!proof && (
                 <button
                   className="btn btn-primary btn-outline m-4"
@@ -125,23 +167,25 @@ const Home: NextPage = () => {
             </button>
           )}
         </div>
-        <div className="bg-base-100 flex items-center flex-col p-5 rounded-2xl">
-          <div className="px-5 text-white">
-            <p className="text-2xl mb-2 text-center">Success 🥳!</p>
-            <p className="text-xl">
-              Please copy the below URL and paste it at{" "}
-              <a
-                href="https://zupass.org/#/add-subscription"
-                className="underline-offset-4 underline"
-                target="_blank"
-                rel="noreferrer"
-              >
-                https://zupass.org/add-subscription
-              </a>
-            </p>
+        {isSigningMessageSuccessfull && isConnected && subscriptionURL && (
+          <div className="bg-base-100 flex items-center flex-col p-5 rounded-2xl">
+            <div className="px-5 text-white">
+              <p className="text-2xl mb-2 text-center">Success 🥳!</p>
+              <p className="text-xl">
+                Please copy the below URL and paste it at{" "}
+                <a
+                  href="https://zupass.org/#/add-subscription"
+                  className="underline-offset-4 underline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  https://zupass.org/add-subscription
+                </a>
+              </p>
+            </div>
+            <AddSubscriptionURL url={subscriptionURL} />
           </div>
-          <AddSubscriptionURL url="https://www.google.com/search?as_q=you+have+to+write+a+really+really+long+search+to+get+to+2000+characters.+like+seriously%2C+you+have+no+idea+how+long+it+has+to+be&as_epq=2000+characters+is+absolutely+freaking+enormous.+You+can+fit+sooooooooooooooooooooooooooooooooo+much+data+into+2000+characters.+My+hands+are+getting+tired+typing+this+many+characters.+I+didn%27t+even+realise+how+long+it+was+going+to+take+to+type+them+all.&as_oq=Argh!+So+many+characters.+I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.+I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.I%27m+bored+now%2C+so+I%27ll+just+copy+and+paste.&as_eq=It+has+to+be+freaking+enormously+freaking+enormous&as_nlo=123&as_nhi=456&lr=lang_hu&cr=countryAD&as_qdr=m&as_sitesearch=stackoverflow.com&as_occt=title&safe=active&tbs=rl%3A1%2Crls%3A0&as_filetype=xls&as_rights=(cc_publicdomain%7Ccc_attribute%7Ccc_sharealike%7Ccc_nonderived).-(cc_noncommercial)&gws_rd=ssl" />
-        </div>
+        )}
       </div>
     </>
   );
